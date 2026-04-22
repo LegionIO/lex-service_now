6a8450ffbfd7bfd9afcbac3d934e25db9dd9e708
Esity <matthewdiverson@gmail.com>
2026-04-22 01:20:24 -0500

    test: add spec_helper and runner test harness

---
+++ spec/spec_helper.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'legion/logging'
+require 'legion/settings'
+require 'legion/cache/helper'
+require 'legion/crypt/helper'
+require 'legion/data/helper'
+require 'legion/json/helper'
+require 'legion/transport/helper'
+
+module Legion
+  module Extensions
+    module Helpers
+      module Lex
+        include Legion::Logging::Helper
+        include Legion::Settings::Helper
+        include Legion::Cache::Helper
+        include Legion::Crypt::Helper
+        include Legion::Data::Helper
+        include Legion::JSON::Helper
+        include Legion::Transport::Helper
+      end
+    end
+
+    module Actors
+      class Every
+        include Helpers::Lex
+      end
+
+      class Once
+        include Helpers::Lex
+      end
+    end
+  end
+end
+
+require 'legion/extensions/service_now'
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.disable_monkey_patching!
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
---
+++ spec/support/runner_test_harness.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module RunnerTestHarness
+  def self.build(*runner_modules)
+    klass = Class.new do
+      include Legion::Extensions::ServiceNow::Helpers::Client
+
+      runner_modules.each { |mod| include mod }
+
+      attr_reader :opts
+
+      def initialize(**opts)
+        @opts = opts
+      end
+
+      def connection(**override)
+        super(**@opts.merge(override))
+      end
+    end
+    klass.new(
+      url: 'https://test.service-now.com',
+      username: 'admin',
+      password: 'secret'
+    )
+  end
+
+  def self.stub_connection
+    stubs = Faraday::Adapter::Test::Stubs.new
+    conn = Faraday.new(url: 'https://test.service-now.com') do |c|
+      c.request :json
+      c.response :json, content_type: /\bjson$/
+      c.adapter :test, stubs
+    end
+    [stubs, conn]
+  end
+end
